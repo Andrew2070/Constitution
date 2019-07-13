d7a2dbd25ffe740a318b08fac9c80c93322a287d
Andrew2070 <flaminsaura11@gmail.com>
2019-07-12 22:00:17 -0700

    Auto-Implement Node Documentation To Wiki
    Gets all possibles nodes, writes them to the wiki if they don't exist.

---
+++ src/main/java/Constitution/Utilities/WikiUtilities.java
@@ -0,0 +1,10 @@
+package constitution.utilities;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WikiUtilities {
+
+	public static final Map<String, String> nodeDescriptions = new HashMap<String, String>();
+	
+}
