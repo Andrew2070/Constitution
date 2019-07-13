d95b0c62226fbbb6f2f14abed11c8efacbb3d8c6
Andrew2070 <flaminsaura11@gmail.com>
2019-07-12 21:58:46 -0700

    Untested Fix For Class Finder Method
    Attempt at finding classes not only in specified package, but within sub-packages as well.

--- src/main/java/Constitution/Utilities/ClassUtilities.java
+++ src/main/java/Constitution/Utilities/ClassUtilities.java
@@ -9,47 +9,36 @@ import java.util.jar.JarInputStream;
 
 public class ClassUtilities {
 	public static final List<Class<?>> getClassesInPackage(String packageName) {
-	    String path = packageName.replaceAll("\\.", File.separator);
-	    List<Class<?>> classes = new ArrayList<>();
-	    String[] classPathEntries = System.getProperty("java.class.path").split(
-	            System.getProperty("path.separator")
-	    );
-
-	    String name;
-	    for (String classpathEntry : classPathEntries) {
-	        if (classpathEntry.endsWith(".jar")) {
-	            File jar = new File(classpathEntry);
-	            try {
-	                JarInputStream is = new JarInputStream(new FileInputStream(jar));
-	                JarEntry entry;
-	                while((entry = is.getNextJarEntry()) != null) {
-	                    name = entry.getName();
-	                    if (name.endsWith(".class")) {
-	                        if (name.contains(path) && name.endsWith(".class")) {
-	                            String classPath = name.substring(0, entry.getName().length() - 6);
-	                            classPath = classPath.replaceAll("[\\|/]", ".");
-	                            classes.add(Class.forName(classPath));
-	                            is.close();
-	                        }
-	                    }
-	                }
-	            } catch (Exception ex) {
-	            }
-	        } else {
-	            try {
-	                File base = new File(classpathEntry + File.separatorChar + path);
-	                for (File file : base.listFiles()) {
-	                    name = file.getName();
-	                    if (name.endsWith(".class")) {
-	                        name = name.substring(0, name.length() - 6);
-	                        classes.add(Class.forName(packageName + "." + name));
-	                    }
-	                }
-	            } catch (Exception ex) {
-	            }
-	        }
-	    }
-	    
-	    return classes;
+		String path = packageName.replaceAll("\\.", File.separator);
+		List<Class<?>> classes = new ArrayList<>();
+		String[] classPathEntries = System.getProperty("java.class.path").split(
+				System.getProperty("path.separator")
+				);
+		String name;
+		for (String classpathEntry : classPathEntries) {
+			try {
+				File base = new File(classpathEntry + File.separatorChar + path);
+				for (File file : base.listFiles()) {
+					name = file.getName();
+					if (name.endsWith(".class")) {
+						name = name.substring(0, name.length() - 6);
+						classes.add(Class.forName(packageName + "." + name));
+					} else {
+						base = new File(classpathEntry + File.separatorChar + path + File.separatorChar + name);
+						if (name.endsWith(".class")) {
+							for (File sub : base.listFiles()) {
+								name = file.getName();
+								if (name.endsWith(".class")) {
+									name = name.substring(0, name.length() - 6);
+									classes.add(Class.forName(packageName + "." + name));
+								}
+							}
+						}
+					}
+				}
+			} catch (Exception ex) {
+			}
+		}
+		return classes;
 	}
 }
