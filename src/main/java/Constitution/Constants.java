35045ce625628280efd764dc551049cc9eb8f9f8
Andrew2070 <flaminsaura11@gmail.com>
2019-07-17 16:59:48 -0700

    Moved Versioning To Constants File

---
+++ src/main/java/Constitution/Constants.java
@@ -0,0 +1,7 @@
+package constitution;
+
+import java.util.List;
+public class Constants {
+	    public static final String MODID   = "constitution";
+	    public static final String VERSION = "@VERSION@";
+}
